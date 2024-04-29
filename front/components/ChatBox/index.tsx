476f03bee4492f9601be05b208ec041071bea20e
easyeun99 <31405000+whitexhanin@users.noreply.github.com>
2024-04-29 17:00:18 +0900

    dm보내기

--- front/components/ChatBox/index.tsx
+++ front/components/ChatBox/index.tsx
@@ -12,16 +12,18 @@ import useSWR from 'swr';
 import useSWRInfinite from 'swr/infinite'
 import { Mention, SuggestionDataItem } from 'react-mentions';
 import gravatar from 'gravatar';
+import useSocket from "@hooks/useSocket";
 
 const ChatBox:VFC = () => {    
     const {workspace , id} = useParams<{workspace : string , id:string}>();    
     const {data : memberData} = useSWR<IUser[]>(`/api/workspaces/${workspace}/members`)
     const { data: chatData, mutate: mutateChat, setSize } = useSWRInfinite<IDM[]>(
-        (index) => `/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=${index + 1}`,
-        fetcher,
+        (index) => `/api/workspaces/${workspace}/dms/${id}/chats`,
+        fetcher, {dedupingInterval:100}
       );
     const [chat, onChangeChat, setChat] = useInput('');    
-    const textareaRef = useRef<HTMLTextAreaElement | null>(null);    
+    const textareaRef = useRef<HTMLTextAreaElement | null>(null);  
+    const [socket ,disconnectSocket]= useSocket(workspace);
 
     useEffect(()=>{
         if (textareaRef.current) {
@@ -29,28 +31,46 @@ const ChatBox:VFC = () => {
           }
     },[])
 
-    const onSubmitForm = useCallback(()=>{
+    const onSubmitForm = useCallback((e)=>{        
+        e.preventDefault();
+        console.log('chatData1',chatData);
         //새로운 dm을 보냈다
         // Socket.io로 노노 서버로 보내는건 login 밖에 없다
         axios.post(`/api/workspaces/${workspace}/dms/${id}/chats`,{
-            content:chat
-        }
-    )
-        .then(()=>{
-            mutateChat();
+            content:chat ,
         })
-        .catch(console.error);        
-    },[]);  
+        .then(()=>{            
+            mutateChat();
+            setChat('');
+        })
+        .catch(console.error);    
+        console.log('chatData2',chatData);    
+    },[workspace , id , chat ]);  
     
 
-    const onKeydownChat  = useCallback(()=>{
+    const onKeydownChat  = useCallback((e)=>{
         console.log('keydown');
-    },[onSubmitForm])
+        console.log(e);
+        if(e.keyCode == 13 && e.shiftKey !== true){
+            onSubmitForm(e);
+        }
+    },[])
+
+    useEffect(()=>{
+        socket?.on('message','message');
+        return()=>{
+            socket?.off('message',chat);
+        }
+    },[chat]);
+    
+    
 
     console.log('?');
     
     return(
-        <>
+        <>  {chatData?.map(chat => {
+            <div>chat</div>
+        })}
             <div className="chatarea">
                 <form onSubmit={onSubmitForm}>
                     <textarea 
